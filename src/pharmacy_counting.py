5c23ee9520f1f89bd08f6850645c5127e9b2e735
praveenk28 <praveenm6@gmail.com>
2018-07-23 17:39:39 -0400

    moved code into functions

--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -1,38 +1,46 @@
 import sys
 import csv
 
-results = {}
+def process_input(input_filename):
 
-input_file = open(sys.argv[1], 'r')
+    results = {}
 
-line = input_file.readline() #skip 1st line#
+    input_file = open(input_filename, 'r')
 
-reader = csv.reader(input_file)
+    line = input_file.readline() #skip 1st line#
 
-for row in reader:
-    drugname = row[3]
-    cost = float (row[4])
-    prescriberid = row[0]
-    if not drugname in results:
-        results[drugname] = {'totalcost':cost, 'prescribers':set()}
-        results[drugname]['prescribers'].add(prescriberid)
-    else:
-        entry = results[drugname]
-        entry['totalcost'] += cost
-        entry['prescribers'].add(prescriberid)
+    reader = csv.reader(input_file)
 
-input_file.close()
+    for row in reader:
+        drugname = row[3]
+        cost = float (row[4])
+        prescriberid = row[0]
+        if not drugname in results:
+            results[drugname] = {'totalcost':cost, 'prescribers':set()}
+            results[drugname]['prescribers'].add(prescriberid)
+        else:
+            entry = results[drugname]
+            entry['totalcost'] += cost
+            entry['prescribers'].add(prescriberid)
 
-output_file = open(sys.argv[2],'w')
+    input_file.close()
 
-output_file.write('drug_name,num_prescriber,total_cost\n')
+    return results
 
-writer = csv.writer(output_file)
-sorted_keys = sorted(results.keys(),key = lambda drugname: (-int(results[drugname]['totalcost']), drugname))
-for drugname in sorted_keys:
-    writer.writerow( [drugname,len(results[drugname]['prescribers']),int(results[drugname]['totalcost'])] )
-output_file.close()
-    
+def create_output(output_filename,results):
 
+    output_file = open(output_filename,'w')
+
+    output_file.write('drug_name,num_prescriber,total_cost\n')
+
+    writer = csv.writer(output_file)
+    sorted_keys = sorted(results.keys(),key = lambda drugname: (-int(results[drugname]['totalcost']), drugname))
+    for drugname in sorted_keys:
+        writer.writerow( [drugname,len(results[drugname]['prescribers']),int(results[drugname]['totalcost'])] )
+    output_file.close()
+        
+
+results = process_input(sys.argv[1])
+create_output(sys.argv[2], results)
 
     
